ee17727cca42d446e269a984311689ec58ff0029
Gustavo Aguiar <gugahoa@gmail.com>
2016-05-18 14:23:30 -0300

    Quest reward container helper for mappers

---
+++ data/actions/scripts/quests/reward.lua
@@ -0,0 +1,44 @@
+function onUse(cid, item, fromPosition, target, toPosition, isHotkey)
+	local chest = Container(item.uid)
+	local player = Player(cid)
+	if not chest or not player then
+		return true
+	end
+
+	local uniqueid = chest:getUniqueId()
+	if player:getStorageValue(uniqueid) == -2 then
+		player:sendTextMessage(MESSAGE_INFO_DESCR, "It is empty.")
+		return true
+	end
+
+	local start = player:getStorageValue(uniqueid) == -1 and 0 or player:getStorageValue(uniqueid)
+	for i = start, chest:getSize() do
+		local reward = chest:getItem(i)
+		if not reward then
+			break
+		end
+
+		if reward:getWeight() > player:getFreeCapacity() then
+			player:sendTextMessage(MESSAGE_INFO_DESCR, 'You have found a ' .. reward:getName() .. ' weighing ' .. reward:getWeight()/100 .. ' oz it\'s too heavy.')
+			player:setStorageValue(uniqueid, i)
+			break
+		else
+			reward = reward:clone()
+			if not reward:moveTo(player) then
+				player:sendTextMessage(MESSAGE_INFO_DESCR, 'You have found a ' .. reward:getName() .. ' weighing ' .. reward:getWeight()/100 .. ' oz it\'s too heavy.')
+				break
+			end
+
+			local reward_msg = reward:getArticle() .. ' ' .. reward:getName()
+			if reward:getCount() > 1 then
+				reward_msg = reward:getCount() .. ' ' .. reward:getPluralName()
+			end
+
+			player:sendTextMessage(MESSAGE_INFO_DESCR, 'You have found ' .. reward_msg .. '.')
+
+			player:setStorageValue(uniqueid, -2)
+		end
+	end
+
+	return true
+end
